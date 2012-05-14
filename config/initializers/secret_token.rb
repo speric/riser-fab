08d8ace0582cc1d8093267271527bb273d15d042
speric <eric@prudentiadigital.com>
2012-05-14 14:47:01 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RiserFab::Application.config.secret_token = 'e2e6a689a6a5377b0b35b15ea8d574f6a230bc7eded17eab98da172a6c297bad3b2275f2ef8fb7e584d52d0124f8fecf217dd316119df8cb8365255e68dec06b'
